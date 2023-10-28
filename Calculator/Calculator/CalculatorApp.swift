d066b2e1cb97ad2085a9a2f19f94d56e8a9c3b18
gurjnderSingh <gurjindersingh663@gmail.com>
2023-10-28 12:50:35 +0530

    inital project creatd

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Gurjinder Singh on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
